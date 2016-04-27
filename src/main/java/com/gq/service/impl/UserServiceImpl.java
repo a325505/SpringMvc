d1f4bd1df809b79c7a36c3d64387dae145a87bc5
liuchao <chaoliu@gqhmt.com>
2016-04-27 11:47:57 +0800

    init

---
+++ src/main/java/com/gq/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.gq.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gq.entity.User;
+import com.gq.mapper.UserMapper;
+
+@Service
+@Transactional 
+@SuppressWarnings("restriction")
+public class UserServiceImpl {
+	@Resource
+	private UserMapper mapper;
+
+	public boolean delete(int id) {
+		
+		return mapper.delete(id);
+	}
+
+	public List<User> findAll() {
+		List<User> findAllList = mapper.findAll();
+		return findAllList;
+	}
+
+	public User findById(int id) {
+
+		User user = mapper.findById(id);
+		
+		return user;
+	}
+
+	public void save(User user) {
+
+		mapper.save(user);
+	}
+
+	public boolean update(User user) {
+
+		return mapper.update(user);
+	}
+}
